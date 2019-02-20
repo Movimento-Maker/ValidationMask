3a9dcb3a78b608f40bdf45f77e26d0b2628f5775
Michel Lobo Esmera dos Santos <msantos.stefanini@desenbahia.ba.gov.br>
2019-02-20 16:35:10 -0300

    Primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+      formulario: FormGroup
+      constructor(private fb: FormBuilder){
+
+      }
+      get f() { return this.formulario.controls; }
+
+      ngOnInit() {
+        this.formulario = this.fb.group({
+          email:['', [Validators.email, Validators.required]],
+          cep:['', Validators.required],
+          telefone:['', [Validators.required, Validators.minLength(11)]]
+        })
+      }
 }
